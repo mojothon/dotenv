89884b34ca00c3977e5705fabaf8d3b62abbf40f
llango <724261350@qq.com>
2024-09-23 12:29:08 +0800

    更新publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# ignore errors because we want to ignore duplicate packages
 for file in output/**/*.conda; do
     magic run rattler-build upload prefix -c "mojo-force" "$file" || true
 done
